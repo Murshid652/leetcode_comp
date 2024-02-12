1dc5d72e40d1b7678b3ea4690eea4566782130bb
Murshid652 <murshidraja652@gmil.com@gmail.com>
2024-02-13 02:48:04 +0530

    formatted the response of profile endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // const express = require('express');
 // const leetcode = require('leetcode-query';
-import {LeetCode} from 'leetcode-query';
-import express from 'express';
+import { LeetCode } from "leetcode-query";
+import express from "express";
 
 const PORT = process.env.PORT || 4000;
 const leetcode = new LeetCode();
@@ -10,25 +10,49 @@ const app = express();
 
 //sma280201
 
+app.get("/profiles", async (req, res) => {
+  try {
+    const user1 = req.query.user1;
+    const user2 = req.query.user2;
 
-app.get('/profiles' , async(req,res) => {
-    try{
-        const user1 = req.query.user1;
-        const user2 = req.query.user2;
-        console.log(user1);
-        const profile1 = await leetcode.user(user1);
-        const profile2 = await leetcode.user(user2);
+    const profile1 = await leetcode.user(user1);
+    const profile2 = await leetcode.user(user2);
 
-        console.log({profile1 , profile2});
+    console.log({ profile1, profile2 });
 
-        res.json({profile1, profile2});
-    }catch (error) {
-        // Handling errors
-        console.error('Error fetching profiles:', error);
-        res.status(500).json({ error: 'Failed to fetch profiles' });
-      }    
-})
+    // res.json({profile1, profile2});
 
-app.listen(PORT , ()=>{
-    console.log(`app is listening on port ${PORT}`);
-})
+    if (profile1.matchedUser && profile2.matchedUser) {
+      const profileData1 = profile1.matchedUser;
+      const response1 = {
+        username: profileData1.username,
+        realName: profileData1.profile.realName,
+        userAvatar: profileData1.profile.userAvatar,
+        countryName: profileData1.profile.countryName,
+        submitStats: profileData1.submitStats,
+        badges: profileData1.badges.map((badge) => ({ icon: badge.icon })),
+      };
+
+      const profileData2 = profile2.matchedUser;
+      const response2 = {
+        username: profileData2.username,
+        realName: profileData2.profile.realName,
+        userAvatar: profileData2.profile.userAvatar,
+        countryName: profileData2.profile.countryName,
+        submitStats: profileData2.submitStats,
+        badges: profileData2.badges.map((badge) => ({ icon: badge.icon })),
+      };
+
+      res.json({response1,response2});
+    } else {
+      throw new Error("username incorrect");
+    }
+  } catch (error) {
+    console.error("Error fetching profiles:", error);
+    res.status(500).json({ error: "Failed to fetch profiles" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
